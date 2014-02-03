52a75122f2e3418979dbdf25736b43f0904ab740
jc9292 <jc92@yopmail.com>
2014-02-03 14:34:57 +0100

    Update javascript.js

--- sources/cssjs/javascript.js
+++ sources/cssjs/javascript.js
@@ -8,7 +8,7 @@ function newajx(func1) {
         if (ajx) {
                 if (func1!='') {
                         ajx.onreadystatechange = function(){
-                        	alert('ready');
+                        	alert('ready:'+ajx.readyState+' '+ajx.status);
                                 if (ajx.readyState==4 && ajx.status==200) { eval(func1+'(ajx);'); }
                         }
                 }
